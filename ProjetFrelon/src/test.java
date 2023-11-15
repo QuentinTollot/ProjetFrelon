7a3bb1e49147cb81104680f4f26a009a62d082b2
MatheoRigolo <lecoqmatheo35@gmail.com>
2023-11-15 10:08:26 +0100

    Premier test de canny

--- ProjetFrelon/src/test.java
+++ ProjetFrelon/src/test.java
@@ -1,11 +1,44 @@
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 public class test {
-        public static void main(String[] args){
-                System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-                Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
-                System.out.println("mat = " + mat.dump());
-        }
+	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+		// Charger la première image de test
+		String imagePath = "ProjetFrelon/data/Trap/hornet01.jpg";
+
+		Mat image = Imgcodecs.imread(imagePath);
+		
+		
+
+		// Creating an empty matrices to store edges, source, destination
+		Mat gray = new Mat(image.rows(), image.cols(), image.type());
+		Mat edges = new Mat(image.rows(), image.cols(), image.type());
+		Mat dst = new Mat(image.rows(), image.cols(), image.type(), new Scalar(0));
+
+		// Converting the image to Gray
+		Imgproc.cvtColor(image, gray, Imgproc.COLOR_RGB2GRAY);
+		
+		// Blurring the image
+		Imgproc.blur(gray, edges, new Size(3, 3));
+		//Imgcodecs.imwrite("ProjetFrelon/data/Results/blur.jpg", edges);
+		
+		// Detecting the edges
+		Imgproc.Canny(edges, edges, 100, 100 * 3);
+		
+		// Copying the detected edges to the destination matrix
+		image.copyTo(dst, edges);
+		
+		
+		
+		// Créer l'image modifiée
+		Imgcodecs.imwrite("ProjetFrelon/data/Results/detected_wings_wBlurr.jpg", dst);
+		Imgcodecs.imwrite("ProjetFrelon/data/Results/gray.jpg", gray);
+
+	}
 }
