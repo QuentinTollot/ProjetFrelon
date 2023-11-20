84e0b8ed232ae8720d401ad4ef221d1f4d8b1175
MatheoRigolo <lecoqmatheo35@gmail.com>
2023-11-20 17:07:47 +0100

    Réorganisation + paramètres à revoir

--- ProjetFrelon/src/test.java
+++ ProjetFrelon/src/test.java
@@ -10,51 +10,45 @@ public class test {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
 		// Charger la première image de test
-		//String imagePath = "ProjetFrelon/data/Trap/hornet01.jpg";
+		// String imagePath = "ProjetFrelon/data/Trap/hornet01.jpg";
 		String imagePath = "ProjetFrelon/data/Trap/hornet01.jpg";
 
 		Mat image = Imgcodecs.imread(imagePath);
-		
+
 		/***************************************
-		 * 		Partie Pré-traitement          *
+		 * Partie Pré-traitement *
 		 ***************************************/
 
-		
-		
-		
-		
-		/***************************************
-		 * 		Partie Edge Detection          *
-		 ***************************************/
-		
-		
 		// Creating an empty matrices to store edges, source, destination
 		Mat contraste = new Mat(image.rows(), image.cols(), image.type());
 		Mat gray = new Mat(image.rows(), image.cols(), image.type());
-		Mat edges = new Mat(image.rows(), image.cols(), image.type());
-		Mat dst = new Mat(image.rows(), image.cols(), image.type(), new Scalar(0));
-		
+		Mat blur = new Mat(image.rows(), image.cols(), image.type());
+
 		// Converting the image to Gray
 		Imgproc.cvtColor(image, gray, Imgproc.COLOR_RGB2GRAY);
-		
+		Imgcodecs.imwrite("ProjetFrelon/data/Results/gray.jpg", gray);
+
+		// Améliorer le contraste
 		Imgproc.equalizeHist(gray, contraste);
-		
+		Imgcodecs.imwrite("ProjetFrelon/data/Results/equalize.jpg", contraste);
+
 		// Blurring the image
-		Imgproc.medianBlur(contraste, edges, 3);
-		//Imgproc.blur(gray, edges, new Size(3, 3));
-		Imgcodecs.imwrite("ProjetFrelon/data/Results/blur.jpg", edges);
+		Imgproc.medianBlur(contraste, blur, 13);
+		// Imgproc.blur(gray, edges, new Size(3, 3));
+		Imgcodecs.imwrite("ProjetFrelon/data/Results/blur.jpg", blur);
 		
+
+		/***************************************
+		 * Partie Edge Detection *
+		 ***************************************/
+
+		Mat dst = new Mat(image.rows(), image.cols(), image.type(), new Scalar(0));
+
 		// Detecting the edges
-		Imgproc.Canny(edges, edges, 100, 100 * 3);
-		
-		// Copying the detected edges to the destination matrix
-		image.copyTo(dst, edges);
-		
-		
-		
+		Imgproc.Canny(blur, dst, 100, 300);
+
 		// Créer l'image modifiée
 		Imgcodecs.imwrite("ProjetFrelon/data/Results/detected_wings.jpg", dst);
-		Imgcodecs.imwrite("ProjetFrelon/data/Results/gray.jpg", gray);
 
 	}
 }
